5c1f73be2bc374b0a923153336495662836d2520
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-05-31 23:47:59 +0200

    Create node.js

---
+++ node.js
@@ -0,0 +1,12 @@
+annuaire-music/
+├── backend/
+│   ├── server.js
+│   ├── routes/
+│   │   └── adresses.js
+│   └── models/
+│       └── Adresse.js
+├── frontend/
+│   ├── index.html
+│   └── script.js
+├── .env
+└── package.json
