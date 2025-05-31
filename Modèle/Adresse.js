3fa9200174347008cf5d29ec61ba1293ab3e4904
inesstlaurent22 <inessaintlaurent@hotmail.com>
2025-06-01 00:01:50 +0200

    Update Adresse.js

--- Modèle/Adresse.js
+++ Modèle/Adresse.js
@@ -11,3 +11,16 @@ const adresseSchema = new mongoose.Schema({
 });
 
 module.exports = mongoose.model('Adresse', adresseSchema);
+const mongoose = require('mongoose');
+
+const adresseSchema = new mongoose.Schema({
+  nom: String,
+  metier: String,
+  adresse: String,
+  code_postal: String,
+  note: Number,
+  description: String,
+  contact: String
+});
+
+module.exports = mongoose.model('Adresse', adresseSchema);
